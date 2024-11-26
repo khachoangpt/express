6e5949298ec1113e8480d588e4248356fdc706cf
Hoang Nguyen <hoangncd3@gmail.com>
2024-11-26 15:29:09 +0700

    chore: update swagger

--- src/modules/auth/controllers/logout.controller.ts
+++ src/modules/auth/controllers/logout.controller.ts
@@ -5,6 +5,28 @@ import type { Request, Response } from 'express'
 import AuthService from '../auth.service'
 import type { LogoutPayload } from '../dtos/logout.payload'
 
+/**
+ * @swagger
+ * /auth/logout:
+ *   post:
+ *     tags:
+ *       - Auth
+ *     summary: Logout a shop
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LogoutPayload'
+ *     responses:
+ *       200:
+ *         description: Logout successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: Logout successfully
+ */
 export default async (req: Request, res: Response) => {
 	const validated = await validator<LogoutPayload>(logoutSchema, req.body)
 	const authService: AuthService = req.scope.resolve(AuthService.resolutionKey)
--- src/modules/auth/dtos/logout.payload.ts
+++ src/modules/auth/dtos/logout.payload.ts
@@ -1,3 +1,15 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LogoutPayload:
+ *       type: object
+ *       required:
+ *         - refreshToken
+ *       properties:
+ *         refreshToken:
+ *           type: string
+ */
 export class LogoutPayload {
 	refreshToken: string
 }
