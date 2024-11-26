2a4451f32306b0c5c6569230e35eb0d279dfd83a
Hoang Nguyen <hoangncd3@gmail.com>
2024-11-26 15:20:39 +0700

    fix: import lodash

--- src/utils/lodash.ts
+++ src/utils/lodash.ts
@@ -1,4 +1,4 @@
-import { type PartialObject, pick as lodashPick } from 'lodash'
+import _, { type PartialObject } from 'lodash'
 
 /**
  * Returns a new object with only the specified keys from the given object.
@@ -7,7 +7,7 @@ import { type PartialObject, pick as lodashPick } from 'lodash'
  * @returns A new object with the picked keys.
  */
 const pick = <T, K extends keyof T>(obj: T, keys: K[]): PartialObject<T> => {
-	return lodashPick(obj, keys)
+	return _.pick(obj, keys)
 }
 
 export { pick }
